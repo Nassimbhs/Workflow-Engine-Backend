be9cfb9c574884f8e67a2b2058fa020b561cfeeb
Nassim Ben Hassine <79005429+Nassimbhs@users.noreply.github.com>
2023-03-02 03:03:38 +0100

    full crud

--- src/main/java/Workflow/example/Workflow/Converter/ActiviteConverter.java
+++ src/main/java/Workflow/example/Workflow/Converter/ActiviteConverter.java
@@ -21,6 +21,7 @@ public class ActiviteConverter {
         dto.setName(activite.getName());
         dto.setCreationDate(activite.getCreationDate());
         dto.setDescription(activite.getDescription());
+        dto.setTypeSortie(activite.getTypeSortie());
         dto.setTacheDtoList(tacheConverter.entityToDto(activite.getTaches()));
         return dto;
     }
