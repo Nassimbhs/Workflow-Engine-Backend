a2289f938416e18f8db8e819c76f882fee6556dd
Nassim Ben Hassine <79005429+Nassimbhs@users.noreply.github.com>
2023-04-05 02:53:08 +0100

    login

--- src/main/java/Workflow/example/Workflow/Controller/AuthController.java
+++ src/main/java/Workflow/example/Workflow/Controller/AuthController.java
@@ -31,9 +31,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     @Autowired
     AuthenticationManager authenticationManager;
